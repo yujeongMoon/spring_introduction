25fd94efa3b99d3a73223f624536dcd21c3492a0
yjmoon <octo2917@gmail.com>
2023-06-04 21:29:42 +0900

    스프링 웹 개발 기초

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 스프링 입문
 [인프런 강의 - 기록 및 정리] 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
 
-### 6월 4일(일)
+### 6월 4일(일)    
 ####프로젝트 환경 설정
     스프링 프로젝트와 테스트 페이지 생성 후 빌드
 
@@ -15,4 +15,18 @@
             > java -jar hello-spring-0.0.1-SNAPSHOT.jar
             > 서버가 실행된다.
             > 브라우저에서 localhost:8080으로 확인!
-    
+
+#### 스프링 웹 개발 기초
+    1. 정적 컨텐츠
+        파일을 그대로 전달
+        > http://localhost:8080/hello-static.html
+        > controller에서 확인했을 때 관련 api가 없으면 resource 경로에서 파일을 찾아 정적 컨텐츠를 전달한다.
+    2. MVC와 템플릿 엔진
+        데이터를 변경하고 템플릿 엔진을 사용하여 전달
+        > MVC(Model View Controller)
+        > view는 화면, controller는 비즈니스 로직 처리, model은 데이터 관리로 분리
+    3. API
+        JSON 형태로 데이터를 전달
+        > @ResponseBody를 설정하면 뷰 이름을 반환하는 것이 아니라 데이터 그대로 전달하도록 인식한다.
+        > HTTP body에 문자내용을 직접 반환
+        > 객체를 생성하여 JSON(key : value) 형태로 데이터를 전달
--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,5 +17,56 @@ public class HelloController {
         * resources/templates 경로의 viewName과 매핑
         */
         return "hello";
+    }
+
+    /*
+    * Get방식으로 api가 호출될 때, parameter로 받아오는 값을 사용한다.
+    * @RequestParam(value = "name", required = false)을 사용하여 name 값을 전달 받는다.
+    * required를 false로 설정하면 값이 없어도 정상 호출이 된다.
+    * http://localhost:8080/hello-mvc?name=spring
+    */
+    @GetMapping("hello-mvc")
+    public String helloMve(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+    
+    /*
+    * @ResponseBody를 설정하면 뷰 이름을 반환하는 것이 아니라 데이터 그대로 전달하도록 인식한다.(HTTP body에 문자내용을 직접 반환)
+    * viewResolver대신에 HttpMessageConverter가 동작한다.
+    * 객체를 반환할 경우, 기본적으로 JSON 방식으로 데이터를 반환한다.
+    *
+    * 기본 문자 처리 : StringHttpMessageConverter
+    * 기본 객체 처리 : MappingJackson2HttpMessageConverter
+    *  
+    */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    /*
+    * 프로퍼티 방식
+    */
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
